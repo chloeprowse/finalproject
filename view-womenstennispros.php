533128a6f6d38d9bebca1f599b21a3a917a6f004
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-04 09:09:24 -0600

    Update view-womenstennispros.php

--- view-womenstennispros.php
+++ view-womenstennispros.php
@@ -2,14 +2,22 @@
 <div class="table-responsive">
   <table class="table">
     <thead>
+      <tr>
       <th>ID</th>
       <th>Name</th>
       <th>Country</th>
+      </tr>
     </thead>
     <tbody>
 <?php
-
+while ($womenstennispro = $womenstennispros ->fetch_assoc()
 ?>
+  <tr>
+    <td><?php echo $womenstennispro['w_tennispro_id']; ?></td>
+    <td></td>
+    <td></td>
+  </tr>
+<?php 
    </tbody>
   </table>
 </div>
