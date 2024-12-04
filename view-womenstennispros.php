23d106b3217a5803bd999526e079e479cc5f60f7
chloeprowse <142910742+chloeprowse@users.noreply.github.com>
2024-12-04 08:58:01 -0600

    Update view-womenstennispros.php

--- view-womenstennispros.php
+++ view-womenstennispros.php
@@ -1,1 +1,15 @@
+<h1>womenstennispros</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Country</th>
+    </thead>
+    <tbody>
+<?php
 
+?>
+   </tbody>
+  </table>
+</div>
